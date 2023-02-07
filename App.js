744e358bed86dc8cec8a67f3b42e3c8719500850
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2023-02-07 14:30:06 -0500

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This will be a glassy calender with a space background</Text>
       <StatusBar style="auto" />
     </View>
   );
