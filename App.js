fe2dd558db95ff466f0be833656e834d9e964793
Reagan Davenport <reagan.a.davenport@gmail.com>
2023-02-07 14:50:02 -0500

    implimented calendarPicker, nothing broke

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CalendarPicker from 'react-native-calendar-picker';
+
+
 
 export default function App() {
+
+  const [selectedStartDate, setSelectedStartDate] = useState(null);
+  const startDate = selectedStartDate ? selectedStartDate.format('YYYY-MM-DD').toString() : '';
+
   return (
     <View style={styles.container}>
       <Text>This will be a glassy calender with a space background</Text>
+      <View>
+        <CalendarPicker onDateChange={setSelectedStartDate}/>
+        <Text style={styles.dateText}>Birthday: {startDate}</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
